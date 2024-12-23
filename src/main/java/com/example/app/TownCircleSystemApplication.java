a46c113eb4d9b13f9564560f5753bc2c48beb5db
milkyway <oooomilkywayoooo@gmail.com>
2024-12-23 14:30:51 +0900

    First_Commit

---
+++ src/main/java/com/example/app/TownCircleSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TownCircleSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TownCircleSystemApplication.class, args);
+	}
+
+}
